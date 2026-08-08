95dfd5ed7a24860deb415bdde2e505a21c67a341
portal222 <dvaportala@gmail.com>
2026-08-08 15:43:43 +0200

    deleting routeway AI

--- src/components/search/Aigenerator.js
+++ src/components/search/Aigenerator.js
@@ -3,19 +3,15 @@ import BackToTop from "../BackToTop";
 import ChatWithGroq from "./ChatWithGroq";
 import ChatWithOpenRouter from "./ChatOpenRouter";
 import AiPollinationImg from "./AiPollinationImg";
-import ChatRouteWay from "./ChatRouteWay";
 import ChatUnoRouter from "./ChatUnoRouter";
 import AiUnoRouterPictures from "./AiUnoRouterPictures";
 
-
 const Aigenerator = () => {
-
 
     return (
         <>
             <ChatUnoRouter />
             <ChatWithOpenRouter />
-            <ChatRouteWay />
             <ChatWithGroq />
             <AiUnoRouterPictures />
             <AiPollinationImg />
